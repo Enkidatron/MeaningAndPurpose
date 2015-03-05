8d75fbab0fd0ebdbb7d0c757f38b2f43d2852c09
Ben S. Thomas <fearlessleaderthomas@gmail.com>
2015-03-05 22:50:06 +0000

    Add User, Response, Answer seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,20 @@ quiz = Quiz.create({family: 0, version: 1})
 questions = quiz.questions.create([{question: "I have a strong <b>sense of meaning and purpose</b> in life", questiontype: 0, family: 0, version: 1},
 	{question: "I have a clear understanding of <b>what</b> gives my life meaning and purpose", questiontype: 0, family: 1, version: 1}])
 
+user = User.new
+user.email = 'test@example.com'
+user.encrypted_password = '$2a$10$AdDatvXeeoyezV3N.SgzyuaD87SwprRZeWMdVxuUL67q8rbep15Au'
+user.password = 'password'
+user.quiz_id = quiz.id
+user.save!
+
+daysago = (3..50).to_a.shuffle.take(10)
+daysago.each do |d|
+	response = Response.create({user_id: user.id, quiz_id: quiz.id})
+	response.created_at = d.days.ago
+	response.save!
+	questions.length.times do |x|
+		answer = Answer.create({textAnswer: "", intAnswer: (1..10).to_a.shuffle.take(1).first, response_id: response.id, question_id: x + 1})
+	end
+end
+
