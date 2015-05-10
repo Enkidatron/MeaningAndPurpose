e1bcaa35731961dd1cb60bfa4cdff86bfdd245ed
Ben S. Thomas <fearlessleaderthomas@gmail.com>
2015-05-10 02:14:51 +0000

    Fix blank default quiz and black responses bugs

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -3,18 +3,20 @@ class GraphsController < ApplicationController
 
 	def user
 		responses = current_user.responses
-		@questions = responses.map{|r| r.quiz.questions}.flatten!.uniq!
-		answerArray = @questions.map do |que| 
-			responses.map do |r| 
-				answer = Answer.where(response: r, question: que).first
-				if answer.nil? then nil else [answer.intAnswer, answer.textAnswer] end
+		unless responses.nil? or responses.length == 0
+			@questions = responses.map{|r| r.quiz.questions}.flatten!.uniq!
+			answerArray = @questions.map do |que| 
+				responses.map do |r| 
+					answer = Answer.where(response: r, question: que).first
+					if answer.nil? then nil else [answer.intAnswer, answer.textAnswer] end
+				end
 			end
+			intAnswerArray = answerArray.each_with_index.map{|aa,i| ["Question #{i+1}"] + aa.map{|a| a[0] unless a.nil?}}
+			textAnswerArray = answerArray.map{|aa| aa.map{|a| a[1] unless a.nil?}}
+			dateArray = ['x'] + responses.map{|r| r.created_at.strftime('%Y-%m-%d')}
+			@data = [dateArray] + intAnswerArray
+			@textData = textAnswerArray
 		end
-		intAnswerArray = answerArray.each_with_index.map{|aa,i| ["Question #{i+1}"] + aa.map{|a| a[0] unless a.nil?}}
-		textAnswerArray = answerArray.map{|aa| aa.map{|a| a[1] unless a.nil?}}
-		dateArray = ['x'] + responses.map{|r| r.created_at.strftime('%Y-%m-%d')}
-		@data = [dateArray] + intAnswerArray
-		@textData = textAnswerArray
 		gon.rabl
 	end
 end
