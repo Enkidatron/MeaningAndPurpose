495d1a418766c498d03333007eff362087d31f26
Ben S. Thomas <fearlessleaderthomas@gmail.com>
2015-05-19 01:41:49 +0000

    Add Admin flag to User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150226034251) do
+ActiveRecord::Schema.define(version: 20150519013900) do
 
   create_table "answers", force: :cascade do |t|
     t.text     "textAnswer"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 20150226034251) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "quiz_id"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
