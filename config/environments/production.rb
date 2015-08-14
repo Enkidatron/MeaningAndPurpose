1627822469bac46a7b15eaddc07620130d225fee
Ben Thomas <fearlessleaderthomas@gmail.com>
2015-08-14 01:44:22 +0000

    Fix email on Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,8 +77,27 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  # Devise setup
+  config.action_mailer.default_url_options = { :host => 'meaningandpurpose.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: ENV["GMAIL_DOMAIN"],
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
+
   # React-rails
   config.react.variant = :production
 
   
 end
+
+Rails.application.routes.default_url_options[:host] = 'meaningandpurpose.herokuapp.com'
