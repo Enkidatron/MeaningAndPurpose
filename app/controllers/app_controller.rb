328c94a98a8283443602330aa89acad125d4a512
Ben Thomas <fearlessleaderthomas@gmail.com>
2016-05-11 01:34:35 +0000

    Randomize Question Order

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -4,7 +4,7 @@ class AppController < ApplicationController
   def home
   	@quiz = current_user.quiz
   	@quiz = Quiz.last if @quiz.nil?
-  	@questions = @quiz.questions
+  	@questions = @quiz.questions.shuffle
     @already_submitted = (Response.where(user_id: current_user.id).where("created_at >= ?", Time.zone.now.beginning_of_day).count > 0)
   	gon.rabl unless @questions.nil? or @quiz.nil?
   end
